bab4e1c47a16228d195716e9e90076c5c1237d7a
ArvinNotDev <arveenjafari19@gmail.com>
2025-09-26 15:08:42 +0330

    adding created_at and updating_at to the accounts model

--- backend-fastapi/apps/accounts/models.py
+++ backend-fastapi/apps/accounts/models.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Column, String, DateTime
-from sqlalchemy.orm import validates
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import validates
 import uuid
+from datetime import datetime
 from utils.validators import validate_and_hash_password, validate_email
 
 Base = declarative_base()
@@ -17,6 +18,8 @@ class Account(Base):
     password = Column(String(255), nullable=False)
     email = Column(String(255), unique=True, nullable=False)
     age = Column(DateTime, nullable=True)
+    created_at = Column(DateTime, default=datetime.timezone.utc, nullable=False)
+    updated_at = Column(DateTime, default=datetime.timezone.utc, onupdate=datetime.timezone.utc, nullable=False)
 
     @validates("password")
     def password_validator(self, key, value):
