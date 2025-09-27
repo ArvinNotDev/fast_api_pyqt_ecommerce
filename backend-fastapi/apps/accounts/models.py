5a29cb9b417dc51a52a83b08dfc9303abb868863
ArvinNotDev <arveenjafari19@gmail.com>
2025-09-27 13:07:17 +0330

    adding role to the accounts model

--- backend-fastapi/apps/accounts/models.py
+++ backend-fastapi/apps/accounts/models.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import validates
 import uuid
 from datetime import datetime, timezone
-from utils.validators import validate_and_hash_password, validate_email
+from utils.validators import validate_and_hash_password, validate_email, validate_role
 
 Base = declarative_base()
 
@@ -18,6 +18,7 @@ class Account(Base):
     password = Column(String(255), nullable=False)
     email = Column(String(255), unique=True, nullable=False)
     birth_date = Column(DateTime, nullable=True)
+    role = Column(String(10), nullable = False, default="MEMBER")
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
     updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc),
                         onupdate=lambda: datetime.now(timezone.utc), nullable=False)
@@ -29,3 +30,7 @@ class Account(Base):
     @validates("email")
     def email_validator(self, key, value):
         return validate_email(value)
+
+    @validates("role")
+    def role_validator(self, key, value):
+        return validate_role(value)
--- backend-fastapi/utils/validators.py
+++ backend-fastapi/utils/validators.py
@@ -2,10 +2,10 @@ from bcrypt import hashpw, gensalt
 import re
 
 
-def validate_and_hash_password(self, key, value):
+def validate_and_hash_password(obj, key, value):
         if len(value) < 8:
             raise ValueError("Password must be at least 8 characters")
-        if value.lower() == self.username.lower():
+        if value.lower() == obj.username.lower():
             raise ValueError("Password cannot be the same as username")
 
         regex = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$"
@@ -19,13 +19,19 @@ def validate_and_hash_password(self, key, value):
         return hashed_password.decode("utf-8")
 
 
-def validate_email(self, key, value):
+def validate_email(obj, key, value):
     email_regex = r"^[\w\.-]+@[\w\.-]+\.\w+$"
     if not re.match(email_regex, value):
         raise ValueError(f"Invalid email address: {value}")
     return value
 
-def validate_price(self, key, value):
+def validate_price(value):
     if value > 0:
         return value
     raise ValueError(f"Invalid price, price can't be {value}")
+
+def validate_role(value):
+     CHOICE = ["MEMBER", "ADMIN", ""]
+     if value in CHOICE:
+          return value
+     return "MEMBER"
